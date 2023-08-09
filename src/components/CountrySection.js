487a472e4e8d7c21397ec0b5227112495403a10b
yaredtt <yaredtekle22@gmail.com>
2023-08-09 18:35:55 +0300

    fix flag in second page

--- src/components/CountrySection.js
+++ src/components/CountrySection.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import getFlagImageUrl from './Flags';
 import PoliticalDivisions from './CountryInfo';
 
 function CountrySection({ geonameId }) {
-  const flagImageUrl = getFlagImageUrl(geonameId);
 
   return (
     <div>
-      <img src={flagImageUrl} alt="country-name" className="flag2" />
       <PoliticalDivisions geonameId={geonameId} />
     </div>
   );
@@ -16,9 +13,6 @@ function CountrySection({ geonameId }) {
 
 CountrySection.propTypes = {
   geonameId: PropTypes.string.isRequired,
-  countryProvinces: PropTypes.shape({
-    countryCode: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default CountrySection;
